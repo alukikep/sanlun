b2c14a861fe81e73a7fd589391cc3c09747dbb43
diharlo <3216371381@qq.com>
2025-01-24 03:30:16 +0800

    一个简单的移动平台的代码

---
+++ Assets/Scripts/MovingPlatform/MovingPlatform.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour//一个简单的代码实现移动平台
+{
+    public float horizonRange;
+    public float verticalRange;
+    public float movingTime;
+    private float movingTimer;
+    private bool isMovingRight;
+    Rigidbody2D rb;
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();           
+    }
+    private void Update()
+    {
+        moving();
+    }
+    private void moving()
+    {
+        float speed = Mathf.Sqrt(horizonRange * horizonRange + verticalRange * verticalRange) / movingTime;        
+        movingTimer -= Time.deltaTime;
+        if(isMovingRight == true)
+        {
+            rb.velocity = new Vector3(horizonRange / movingTime,  verticalRange / movingTime).normalized * speed;
+            if (movingTimer <= 0)
+            {
+                movingTimer = movingTime;
+                isMovingRight = false;
+            }
+            
+        }
+        if (isMovingRight == false)
+        {
+            rb.velocity = new Vector3(-horizonRange / movingTime, -verticalRange / movingTime).normalized * speed;
+            if (movingTimer <= 0)
+            {
+                movingTimer = movingTime;
+                isMovingRight = true;
+            }
+
+        }
+
+    }
+}
