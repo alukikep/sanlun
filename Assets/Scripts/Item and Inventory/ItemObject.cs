11a8762789f3887e93b4273d6f7a41e0fa1b6f5e
stardusthyc145 <2551320941@qq.com>
2025-02-23 23:12:07 +0800

    修改了HealthMaxPotion的逻辑
    目前还存在一点问题

--- Assets/Scripts/Item and Inventory/ItemObject.cs
+++ Assets/Scripts/Item and Inventory/ItemObject.cs
@@ -5,20 +5,28 @@ using UnityEngine;
 public class ItemObject : MonoBehaviour
 {
    
-    [SerializeField] private ItemData ItemData;
+    [SerializeField] private ItemData ItemData1;
 
     private void OnValidate()
     {
-        GetComponent<SpriteRenderer>().sprite =ItemData.icon;
-        gameObject.name ="Item object-"+ ItemData.itemName;
+        GetComponent<SpriteRenderer>().sprite =ItemData1.icon;
+        gameObject.name ="Item object-"+ ItemData1.itemName;
     }
    
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.GetComponent<Player>() != null)
         {
-            Inventory.Instance.AddItem(ItemData);
-            Destroy(gameObject);
+            if (ItemData1.itemType == ItemData.ItemType.HealthMaxPotion || ItemData1.itemType == ItemData.ItemType.EnternalAttackPotion)
+            {
+                Inventory.Instance.UseItem(ItemData1);
+                Destroy(gameObject);
+            }
+            else
+            {
+                Inventory.Instance.AddItem(ItemData1);
+                Destroy(gameObject);
+            }
         }
     }
 }
