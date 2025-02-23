7227364a921c5d597903b8fb5b820c9558ae03f1
AlucikeP <1745132070@qq.com>
2025-02-23 20:11:25 +0800

    新增怪物，牛头人

--- Assets/Scripts/boss/Dracula/FireBall.cs
+++ Assets/Scripts/boss/Dracula/FireBall.cs
@@ -5,22 +5,36 @@ using UnityEngine;
 public class FireBall : MonoBehaviour
 { 
   private Rigidbody2D rigidbody2D;
+    private SpriteRenderer spriteRenderer;
 
 [SerializeField] private float moveSpeed;
 private float destroyTimer = 10;//×Ô»ÙÊ±¼ä
 public float ATK;
     private GameObject player;
 // Start is called before the first frame update
-void Start()
+void Awake()
 {
+        spriteRenderer = GetComponent<SpriteRenderer>();
         player = GameObject.FindWithTag("Player");
         float movingDir = player.transform.position.x > transform.position.x ? 1 : -1;
+        if(movingDir > 0)
+        {
+            spriteRenderer.flipX = true;
+        }
     rigidbody2D = GetComponent<Rigidbody2D>();
     rigidbody2D.velocity =new Vector2(moveSpeed*movingDir, 0);
 }
 
-// Update is called once per frame
-void Update()
+    private void Start()
+    {
+        if(rigidbody2D.velocity.x>0)
+        {
+            spriteRenderer.flipX = false;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
 {
     destroyTimer -= Time.deltaTime;
     if (destroyTimer < 0)
