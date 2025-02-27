38796d65bf348409d78f95556caef3ea28a8a306
stardusthyc145 <2551320941@qq.com>
2025-02-27 09:56:16 +0800

    引入中文字体，完善存档位逻辑

--- Assets/Scripts/Panel/SaveLoadPanel.cs
+++ Assets/Scripts/Panel/SaveLoadPanel.cs
@@ -1,23 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using TMPro;
-using UnityEngine.SceneManagement;
+
 
 public class SaveLoadPanel : MonoBehaviour
 {
     private GameObject panel;
     public GameObject checkPanel;
-    public GameObject text;
-    private TextMeshProUGUI textMeshPro;
     private void Start()
     {
         panel = gameObject;
-        // 获取当前物体的子物体
-        textMeshPro = text.GetComponent<TextMeshProUGUI>();
-        // 获取当前场景的名称
-        string currentSceneName = SceneManager.GetActiveScene().name;
-        textMeshPro.text = currentSceneName;
+        
     }
     private void Update()
     {
--- Assets/Scripts/Panel/Slot.cs
+++ Assets/Scripts/Panel/Slot.cs
@@ -1,10 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
 
 public class Slot : MonoBehaviour
 {
-    public GameObject parentPanel; 
+    public GameObject parentPanel;
+    private TextMeshProUGUI sceneName;
+    private void Start()
+    {
+        //获取父物体
+        Transform parentTransform = transform.parent;
+        parentPanel=transform.parent.gameObject;
+        // 获取当前物体的子物体
+        sceneName = GetComponentInChildren<TextMeshProUGUI>();
+        // 获取当前场景的名称
+        string currentSceneName = SceneManager.GetActiveScene().name;
+        sceneName.text = currentSceneName;
+    }
     public void SaveGame()
     {
         Player.Instance.SavePlayer();
