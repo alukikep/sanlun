4d1190f3974e2628b05449c848c03580b9b76a57
diharlo <3216371381@qq.com>
2025-03-02 11:42:37 +0800

    修bug

--- Assets/Scripts/Panel/StartGameButton.cs
+++ Assets/Scripts/Panel/StartGameButton.cs
@@ -6,9 +6,25 @@ using UnityEngine.SceneManagement;
 public class StartGameButton : MonoBehaviour
 {
     public GameObject Inventory;
+    private GameObject playerParent;
+    private void Start()
+    {
+        playerParent = GameObject.Find("PlayerParent");
+    }
     public void StartGame()
     {
+
         SceneManager.LoadScene("CastleHall");
+        GameObject[] allObj = Resources.FindObjectsOfTypeAll<GameObject>();
+        foreach(GameObject obj in allObj)
+        {
+            if(obj.name =="Player")
+            {
+                obj.SetActive(true);
+              
+            }
+        }
         Inventory.SetActive(true);
     }
+   
 }
